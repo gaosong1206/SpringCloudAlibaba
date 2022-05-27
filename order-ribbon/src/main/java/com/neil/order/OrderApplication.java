684b221e51b91046eaeb72756fc58203eb8a47c7
neil <865380824@qq.com>
2022-05-27 17:57:14 +0800

    202205271757

---
+++ order-ribbon/src/main/java/com/neil/order/OrderApplication.java
@@ -0,0 +1,31 @@
+package com.neil.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+//@RibbonClients(value = {
+//        @RibbonClient(name = "stock-nacos",configuration = RibbonRandomRuleConfig.class)
+//})
+public class OrderApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(OrderApplication.class,args);
+
+    }
+
+    @Bean
+    @LoadBalanced // 增加负载均衡调用机制，默认采用轮训负载机制，默认采用ribbon的负载均衡机制
+    public RestTemplate restTemplate(RestTemplateBuilder builder) {
+        RestTemplate restTemplate = builder.build();
+        return restTemplate;
+
+    }
+
+
+}
---
+++ stock-nacos/src/main/java/com/neil/stock/controller/StockController.java
@@ -0,0 +1,22 @@
+package com.neil.stock.controller;
+
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/stock")
+public class StockController {
+
+    @Value("${server.port}")
+    private Integer port;
+
+    @RequestMapping("/reduce")
+    public String reduce(){
+        System.out.println("扣减库存");
+        return "扣减库存:"+port;
+    }
+
+
+}
